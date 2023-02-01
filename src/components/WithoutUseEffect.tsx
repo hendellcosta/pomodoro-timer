2479bf63f44f7650927e237419517789bcf3ae92
hendellcosta <discordstests@gmail.com>
2023-02-01 16:51:11 +0000

    ⭐ Star the repository!

---
+++ src/components/WithoutUseEffect.tsx
@@ -0,0 +1,72 @@
+import { useState } from "react";
+import { RiPauseMiniFill, RiPlayMiniFill } from "react-icons/ri";
+import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+function AppClone() {
+  const [minutesValue, setMinutesValue] = useState(25);
+  const [secondsValue, setSecondsValue] = useState(0);
+
+  const [isPaused, setIsPaused] = useState(false);
+  const [intervalId, setIntervalId] = useState(null);
+  const percentage = Math.round((minutesValue / 60) * 100);
+  const [displayInfo, setDisplayInfo] = useState(false);
+
+  const minutes = minutesValue < 10 ? `0${minutesValue}` : minutesValue;
+  const seconds = secondsValue < 10 ? `0${secondsValue}` : secondsValue;
+
+  function handlePause() {
+    setIsPaused(!isPaused);
+  }
+
+  function updateTimer() {
+    if (isPaused) {
+      if (secondsValue === 0) {
+        if (minutesValue !== 0) {
+          setSecondsValue(59);
+          setMinutesValue(minutesValue - 1);
+        } else {
+          let minutesValue = displayInfo ? 24 : 4;
+          let secondsValue = 59;
+
+          setMinutesValue(minutesValue);
+          setSecondsValue(secondsValue);
+          setDisplayInfo(!displayInfo);
+        }
+      } else {
+        setSecondsValue(secondsValue - 1);
+      }
+    }
+  }
+
+  setInterval(updateTimer, 1000);
+
+  return (
+    <main className="flex items-center justify-center h-[100vh] w-[100vw] absolute">
+      <div className="text-6xl flex flex-col gap-16">
+        <div className="flex flex-col gap-6 justify-center items-center">
+          <CircularProgressbar
+            value={percentage}
+            text={`${minutes}:${seconds}`}
+            styles={buildStyles({
+              textColor: "#fff",
+              pathColor: "#eee",
+              trailColor: "#3e98c7",
+            })}
+          />
+
+          <button
+            onClick={handlePause}
+            className="border rounded-full bg-white text-[#30384b]"
+          >
+            {!isPaused ? <RiPlayMiniFill /> : <RiPauseMiniFill />}
+          </button>
+
+          {displayInfo && <p>Break Time!</p>}
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default AppClone;
